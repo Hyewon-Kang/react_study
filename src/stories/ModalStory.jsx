68e7773a22025e2afd44c8ae913e047ab8be6222
Hyewon-Kang <gpdnjs22@@>
2020-05-12 07:25:45 +0900

    #6.4

---
+++ src/stories/ModalStory.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Modal from "../06/Modal";
+import Button from "../04/Button";
+import Text from "../04/Text";
+import ButtonWithModal from "../06/ButtonWithModal";
+import ModalProvider, { Consumer } from "../06/ModalProvider";
+
+storiesOf("Modal", module)
+    .addWithJSX("기본 설정", () => (
+        <Modal>
+            <div>
+                <Text>
+                    정말로 삭제하시겠습니까?
+                </Text>
+            </div>
+            <Button primary>예</Button>
+            <Button>닫기</Button>
+        </Modal>
+    ))
+    .addWithJSX("ButtonWithModal", () => <ButtonWithModal />)
+    .addWithJSX("ModalProvider", () => (
+        <ModalProvider>
+            <div>
+                <text>다음 버튼을 눌러 모달을 실행합니다.</text>
+                <Consumer>
+                    {({ openModal }) => <Button onPress={() => openModal()}>삭제</Button>}
+                </Consumer>
+            </div>
+        </ModalProvider>
+    ));
